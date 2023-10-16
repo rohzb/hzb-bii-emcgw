b9d00a091c5b06346284470dd59425d13831bd5d
Ruslan Ovsyannikov <90385715+rohzb@users.noreply.github.com>
2023-10-16 08:52:03 +0000

    refactoring app and config handler

--- src/emcgw/app.py
+++ src/emcgw/app.py
@@ -22,37 +22,54 @@ class ConfigHandler:
             "listen_port": 8080,
             "connect_port": 80,
             "log_level": "INFO",
+            "allowed_clients" : None
         }
 
-        self.parser = argparse.ArgumentParser(prog="emcgw")
-        self.parser.add_argument("-c", "--config", help="Path to YAML config file")
-        self.parser.add_argument("-l", "--listen-host", help="The host to listen on")
-        self.parser.add_argument("-p", "--listen-port", type=int, help="The port to bind to")
-        self.parser.add_argument("-r", "--connect-host", help="The target host to connect to")
-        self.parser.add_argument("-P", "--connect-port", type=int, help="The target port to connect to")
-        self.parser.add_argument("-L", "--log-level", choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL", "VERBOSE", "TRACE"], help="Set logging level")
-        self.parser.add_argument("-v", dest="verbosity", action="count", default=0, help="Increase verbosity level (-v for INFO, -vv for DEBUG, -vvv for TRACE)")
-        self.parser.add_argument("-a", "--allowed-clients", nargs="*", help="List of allowed clients in IP/CIDR or hostname format")
         self.args = None
         self.config = {}
 
-    def parse(self):
+    def process(self):
         """
-        Parse command-line arguments and load configuration from files.
+        Parse command-line arguments and load configuration settings.
 
-        This method parses command-line arguments and loads configuration data from files. It also processes command-line
-        arguments to set configuration options.
+        This method parses command-line arguments, loads configuration data from files, and applies command-line arguments
+        to set configuration options.
 
         Args:
             None
 
         Returns:
-            Tuple[argparse.Namespace, dict]: Parsed command-line arguments and merged configuration.
+            dict: Merged configuration settings.
         """
-        self.args = self.parser.parse_args()
+        self.parse_command_line_args()
         self.load_config_from_files()
-        self.process_args()
-        return self.args, self.config
+        self.apply_command_line_args()
+        return self.config
+
+    def parse_command_line_args(self):
+        """
+        Configure and parse command-line arguments, including loading configuration from files.
+
+        This method configures the argument parser, parses command-line arguments, and loads configuration data from files.
+        It also processes command-line arguments to set configuration options.
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
+        parser = argparse.ArgumentParser(prog="emcgw")
+        parser.add_argument("-c", "--config", help="Path to YAML config file")
+        parser.add_argument("-l", "--listen-host", help="The host to listen on")
+        parser.add_argument("-p", "--listen-port", type=int, help="The port to bind to")
+        parser.add_argument("-r", "--connect-host", help="The target host to connect to")
+        parser.add_argument("-P", "--connect-port", type=int, help="The target port to connect to")
+        parser.add_argument("-L", "--log-level", choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL", "VERBOSE", "TRACE"], help="Set logging level")
+        parser.add_argument("-v", dest="verbosity", action="count", default=0, help="Increase verbosity level (-v for INFO, -vv for DEBUG, -vvv for TRACE)")
+        parser.add_argument("-a", "--allowed-clients", nargs="*", help="List of allowed clients in IP/CIDR or hostname format")
+
+        self.args = parser.parse_args()
 
     def load_config_from_files(self):
         """
@@ -76,9 +93,9 @@ class ConfigHandler:
                     if config_data:
                         self.config.update(config_data)
 
-    def process_args(self):
+    def apply_command_line_args(self):
         """
-        Process command-line arguments.
+        Process command-line arguments and apply configuration settings.
 
         This method processes command-line arguments to set configuration options. It handles setting logging levels,
         adjusting verbosity, and updating the log_level based on command-line arguments.
@@ -94,22 +111,20 @@ class ConfigHandler:
             "listen_port": self.args.listen_port or self.config.get("listen_port"),
             "connect_host": self.args.connect_host or self.config.get("connect_host"),
             "connect_port": self.args.connect_port or self.config.get("connect_port"),
+            "allowed_clients" : self.args.allowed_clients or self.config.get("allowed_clients")
         })
-
-        allowed_clients = self.args.allowed_clients or self.config.get("allowed_clients")
-        self.config["allowed_clients"] = allowed_clients
 
         log_level = self.args.log_level or self.config.get("log_level", "INFO")
 
         # Process verbosity to set log_level
-        if self.args.verbosity == 0:
-            log_level = "INFO"
-        elif self.args.verbosity == 1:
-            log_level = "VERBOSE"
-        elif self.args.verbosity == 2:
-            log_level = "DEBUG"
-        elif self.args.verbosity >= 3:
-            log_level = "TRACE"
+        # if verbosity is 0 consider it was not set and log_level should not change
+        if self.args.verbosity != 0:
+            if self.args.verbosity == 1:
+              log_level = "VERBOSE"
+            elif self.args.verbosity == 2:
+                log_level = "DEBUG"
+            elif self.args.verbosity >= 3:
+                log_level = "TRACE"
 
         self.config["log_level"] = log_level
 
@@ -146,13 +161,15 @@ def main():
         None
     """
     config_handler = ConfigHandler()
-    args, config = config_handler.parse()
+    config = config_handler.process()
 
     # Initialize the custom logger
     logger.set_log_level(config["log_level"])
-    
 
+    # preprocess access list
     access_list = AccessList(config["allowed_clients"])
+
+    # start the server
     server = Server(config["listen_host"], config["listen_port"], config["connect_host"], config["connect_port"], access_list)
     server.start()
 
